65b0fc3d10d9ee62103b89ea228dd3c7686f90d5
Simon Wallner <me@simonwallner.at>
2011-08-13 21:23:08 +0200

    project setup seems to work (except for product name problem)

--- include/Context.hpp
+++ include/Context.hpp
@@ -1,10 +1,15 @@
 #ifndef KOCMOC_CORE_CONTEXT_HPP
 #define KOCMOC_CORE_CONTEXT_HPP
 
+#include <iostream.h>
+
 namespace kocmoc {
 	namespace core {
 		
-		void init(void);
+		void init(void)
+		{
+			std::cout << "init context..." << std::endl;
+		}
 	}
 }
 
