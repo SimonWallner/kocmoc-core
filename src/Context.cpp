1952789ca815691ad3aa4f6bf056421b56ecbf2d
Simon Wallner <me@simonwallner.at>
2011-08-14 11:11:46 +0200

    small fixes to build again. commented out glfw for that purpose.

--- src/Context.cpp
+++ src/Context.cpp
@@ -1,8 +1,11 @@
 #include "Context.hpp"
 
 #include <iostream.h>
+#include <gitSHA1.h>
 
 void kocmoc::core::createContext(void) 
 {
 	std::cout << "creating context now..." << std::endl;
+	std::cout << __DATE__ << std::endl;
+	std::cout << KOCMOC_CORE_GIT_SHA1 << std::endl;
 }
