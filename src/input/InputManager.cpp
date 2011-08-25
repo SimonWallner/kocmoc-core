c48b634edfe4553782d6d6c9bf659f9e0c8c655f
Simon Wallner <me@simonwallner.at>
2011-08-25 17:50:35 +0200

    added BR symbol implementation from the kocmoc demo and ditched the keyboard listener

--- src/input/InputManager.cpp
+++ src/input/InputManager.cpp
@@ -2,6 +2,8 @@
 
 #include <kocmoc-core/input/KeyboardListener.hpp>
 
+#include <string>
+
 using namespace kocmoc::core::input;
 
 InputManager::InputManager(GLFWwindow _windowHandle)
@@ -17,18 +19,7 @@ void InputManager::addKeyboardListener(KeyboardListener *listener)
 
 void InputManager::poll(void)
 {
-	int keys[6] = {GLFW_KEY_ESCAPE, GLFW_KEY_A, GLFW_KEY_S, GLFW_KEY_D, GLFW_KEY_W};
-	
-	for (unsigned int i = 0; i < 6; i++)
-	{
-		if (glfwGetKey(windowHandle, keys[i]) == GLFW_KEY_DOWN)
-		{
-			for (KeyboardListenerList::iterator it = keyboardListenerList.begin(); it != keyboardListenerList.end(); it++)
-			{
-				(*it).keyPressed(keys[i]);
-			}
-		}
-	}
+	;
 }
 
 
