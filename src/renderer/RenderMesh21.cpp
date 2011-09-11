68e55447bd9b38626c73574fbc621e0daaebad30
Simon Wallner <me@simonwallner.at>
2011-09-11 18:39:49 +0200

    started to work on the gl code. more research needed.

--- src/renderer/RenderMesh21.cpp
+++ src/renderer/RenderMesh21.cpp
@@ -2,13 +2,24 @@
 
 #include <iostream>
 
+#include <GL/glfw3.h>
+
 using namespace kocmoc::core::renderer;
 
 using kocmoc::core::scene::Camera;
 
 void RenderMesh21::prepare(void)
 {
-	;
+	GLuint buffer = 0;
+	
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 
+				 3 * triangleMesh->vertexCount * sizeof(float), 
+				 triangleMesh->vertexPositions, 
+				 GL_STATIC_DRAW);
+
+	glVertexAttrib3fv(0, NULL);
 }
 
 void RenderMesh21::draw(Camera *camera)
